d900969b359c404f99ca7843e91d7b8e1b6f0bcc
Julia Atayde <juliaatayde@julias-mbp.mynetworksettings.com>
2024-06-19 08:47:31 -0400

    Add third pass

--- metia/expand_dates.py
+++ metia/expand_dates.py
@@ -12,21 +12,27 @@ data['Fecha Partida (Mes/Dia/Año)'] = pd.to_datetime(data['Fecha Partida (Mes/D
 # Define a function to expand rows
 def expand_rows(row):
     expanded = []
-    start_date = row['Fecha Llegada \n(Mes/Dia/Año)']
-    end_date = row['Fecha Partida (Mes/Dia/Año)']
-    
-    if pd.isna(start_date) and pd.isna(end_date):
-        expanded.append(row)
-    else:
-        if pd.isna(start_date):
-            start_date = end_date
-        if pd.isna(end_date):
-            end_date = start_date
-        
-        for single_date in pd.date_range(start_date, end_date):
+    if row['Porcion Viaje'] == 1:
+        if pd.notna(row['Fecha Partida (Mes/Dia/Año)']) and row['Fecha Partida (Mes/Dia/Año)'].year >= 2020:
             new_row = row.copy()
-            new_row['Fecha'] = single_date
+            new_row['Fecha'] = row['Fecha Partida (Mes/Dia/Año)']
             expanded.append(new_row)
+    else:
+        start_date = row['Fecha Llegada \n(Mes/Dia/Año)']
+        end_date = row['Fecha Partida (Mes/Dia/Año)']
+        
+        if pd.isna(start_date) and pd.isna(end_date):
+            expanded.append(row)
+        else:
+            if pd.isna(start_date):
+                start_date = end_date
+            if pd.isna(end_date):
+                end_date = start_date
+            
+            for single_date in pd.date_range(start_date, end_date):
+                new_row = row.copy()
+                new_row['Fecha'] = single_date
+                expanded.append(new_row)
     
     return expanded
 
@@ -48,7 +54,7 @@ expanded_data = expanded_data.drop(columns=['Fecha Llegada \n(Mes/Dia/Año)', 'F
 
 # Select only the required columns in the desired order and rename them accordingly
 expanded_data = expanded_data[['Folio', 'Fecha', 'Latitude', 'Longitude', 'Elevation (meters)', 
-                               'Pueblo, Municipio Partida', 'País', 'Horas_en_el_Lugar', 'Modo_de_Transporte', 'Comentarios']]
+                               'Pueblo, Municipio Partida', 'País', 'Tiempo Ubicacion', 'Metodo de Transporte (Tiempo en Transito)', 'Comentarios']]
 
 expanded_data.columns = ['ID (Folio)', 'Date', 'Latitude', 'Longitude', 'Elevation (meters)', 'City/Town', 
                          'Country', 'Hours at Location', 'Transit Mode (as Departed from this column\'s location)', 'comments']
